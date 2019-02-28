1ae285eb86a47cabcb4d8d73562b82ece4f254ee
Anton Briganti <anton.briganti@myob.com>
2019-02-28 15:03:19 +1100

    removed unused setters

--- src/main/java/GridCoordinates.java
+++ src/main/java/GridCoordinates.java
@@ -11,15 +11,9 @@ public class GridCoordinates {
         return row;
     }
 
-    public void setRow(int row) {
-        this.row = row;
-    }
 
     public int getCol() {
         return col;
     }
 
-    public void setCol(int col) {
-        this.col = col;
-    }
 }
